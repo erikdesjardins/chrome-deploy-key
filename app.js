0c8fb73d246bfd7af3ef7eb1aaf748b6b54cfac9
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-12-05 13:01:52 -0500

    remove jquery

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-$(() => {
-  const clientId = $('#client_id');
-  const clientSecret = $('#client_secret');
-  const codeLink = $('#code_link');
-  const code = $('#code');
-  const curl = $('#curl');
+const clientId = document.getElementById('client_id');
+const clientSecret = document.getElementById('client_secret');
+const codeLink = document.getElementById('code_link');
+const code = document.getElementById('code');
+const curl = document.getElementById('curl');
 
-  clientId.on('input', update);
-  clientSecret.on('input', update);
-  code.on('input', update);
+clientId.addEventListener('input', update);
+clientSecret.addEventListener('input', update);
+code.addEventListener('input', update);
 
-  function update() {
-    clientId.css('border-color', 'red');
-    clientSecret.css('border-color', 'red');
-    codeLink.removeAttr('href');
-    code.css('border-color', 'red');
-    curl.val('');
+function update() {
+  clientId.style.borderColor = 'red';
+  clientSecret.style.borderColor =' red';
+  codeLink.removeAttrbute('href');
+  code.style.borderColor = 'red';
+  curl.value = '';
 
-    if (clientId.val()) {
-      clientId.css('border-color', 'green');
-      codeLink.attr('href', `https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/chromewebstore&client_id=${clientId.val()}&redirect_uri=urn:ietf:wg:oauth:2.0:oob`);
-    }
-    
-    if (clientSecret.val()) {
-      clientSecret.css('border-color', 'green');
-    }
-
-    if (code.val()) {
-      code.css('border-color', 'green');
-    }
-
-    if (clientId.val() && clientSecret.val() && code.val()) {
-      curl.val([
-        `curl 'https://accounts.google.com/o/oauth2/token' -s -d 'client_id=${clientId.val()}&client_secret=${clientSecret.val()}&code=${code.val()}&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob'`,
-        `grep '"refresh_token"'`,
-        `sed -r s/'^.*"refresh_token"[^:]*:[^:]*"([^"]+)".*$'/\\\\1/`,
-      ].join(' | '));
-    }
+  if (clientId.value) {
+    clientId.style.borderColor = 'green';
+    codeLink.href = `https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/chromewebstore&client_id=${clientId.value}&redirect_uri=urn:ietf:wg:oauth:2.0:oob`;
   }
 
-  update();
-});
+  if (clientSecret.value) {
+    clientSecret.style.borderColor = 'green';
+  }
+
+  if (code.value) {
+    code.style.borderColor = 'green';
+  }
+
+  if (clientId.value && clientSecret.value && code.value) {
+    curl.value = [
+      `curl 'https://accounts.google.com/o/oauth2/token' -s -d 'client_id=${clientId.value}&client_secret=${clientSecret.value}&code=${code.value}&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob'`,
+      `grep '"refresh_token"'`,
+      `sed -r s/'^.*"refresh_token"[^:]*:[^:]*"([^"]+)".*$'/\\\\1/`,
+    ].join(' | ');
+  }
+}
+
+update();
+
